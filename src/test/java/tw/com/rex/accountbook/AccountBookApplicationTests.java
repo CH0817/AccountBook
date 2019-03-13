ded377c7bb7bdcbf60d594320ff1df2dbc735fd1
Rex Yu <yu.chenhang@gmail.com>
2019-03-13 17:01:26 +0800

    Setting Spring Boot test

--- src/test/java/tw/com/rex/accountbook/AccountBookApplicationTests.java
+++ src/test/java/tw/com/rex/accountbook/AccountBookApplicationTests.java
@@ -1,13 +1,13 @@
 package tw.com.rex.accountbook;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 //使用 Cucumber 測試
 @RunWith(Cucumber.class)
-// TODO 未定義
 // glue: 指定 feature 文件位置
 // features: 指定 test class package
-// @CucumberOptions(glue = {"tw.com.rex.cucumber.test"}, features = {"src/test/resources/features"})
+@CucumberOptions(glue = {"tw.com.rex.accountbook"}, features = {"src/test/resources/features"})
 public class AccountBookApplicationTests {}
 
