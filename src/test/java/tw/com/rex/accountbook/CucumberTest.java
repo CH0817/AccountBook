bc80ef6df34deeab4952d02359be86afeb62e3c4
Rex Yu <yu.chenhang@gmail.com>
2019-03-13 17:10:15 +0800

    Create Given user on the register page test method.

--- src/test/java/tw/com/rex/accountbook/CucumberTest.java
+++ src/test/java/tw/com/rex/accountbook/CucumberTest.java
@@ -1,5 +1,6 @@
 package tw.com.rex.accountbook;
 
+import cucumber.api.java.en.Given;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,6 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)//指定使用 Spring test
 @SpringBootTest //必添加，否則無法注入 bean
@@ -16,5 +22,13 @@ public class CucumberTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Given("user on the register page")
+    public void userOnTheRegisterPage() throws Exception {
+        this.mockMvc.perform(get("/register"))//
+                .andExpect(status().isOk())//
+                .andExpect(view().name("register"))//
+                .andDo(print());
+    }
 
 }
