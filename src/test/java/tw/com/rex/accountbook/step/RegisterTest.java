40fe704eb237ebf6f547aca69f2d39b8de78f3c5
Rex Yu <yu.chenhang@gmail.com>
2019-03-15 15:31:52 +0800

    Create RegisterTest cucumber mapping

---
+++ src/test/java/tw/com/rex/accountbook/step/RegisterTest.java
@@ -0,0 +1,47 @@
+package tw.com.rex.accountbook.step;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import tw.com.rex.accountbook.step.base.BaseTest;
+
+public class RegisterTest extends BaseTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Given("^user on the register page$")
+    public void userOnTheRegisterPage() {
+        System.out.println("mvc: " + mvc);
+    }
+
+    @When("^a user enter a not registered email and length (\\d+) to (\\d+) password$")
+    public void aUserEnterANotRegisteredEmailAndLengthToPassword(int arg0, int arg1) {
+    }
+
+    @Then("^show message to user$")
+    public void showMessageToUser() {
+    }
+
+    @And("^store user information$")
+    public void storeUserInformation() {
+    }
+
+    @And("^forward to main page$")
+    public void forwardToMainPage() {
+
+    }
+
+    @When("^a user enters registered email$")
+    public void aUserEntersRegisteredEmail() {
+
+    }
+
+    @When("^a user enters a length of the password outside of (\\d+) to (\\d+)$")
+    public void aUserEntersALengthOfThePasswordOutsideOfTo(int arg0, int arg1) {
+
+    }
+}
