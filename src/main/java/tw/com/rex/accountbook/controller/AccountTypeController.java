cbec08e4526383101837a2b850e2acd7f1151baf
Rex Yu <yu.chenhang@gmail.com>
2019-03-21 17:15:12 +0800

    增加針對 Controller 的測試

---
+++ src/main/java/tw/com/rex/accountbook/controller/AccountTypeController.java
@@ -0,0 +1,31 @@
+package tw.com.rex.accountbook.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tw.com.rex.accountbook.repository.dao.AccountType;
+import tw.com.rex.accountbook.service.AccountTypeService;
+
+@RestController
+@RequestMapping("/accountType")
+public class AccountTypeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private AccountTypeService accountTypeService;
+
+    @Autowired
+    public AccountTypeController(AccountTypeService accountTypeService) {
+        this.accountTypeService = accountTypeService;
+    }
+
+    @GetMapping("/find/name/{name}")
+    public AccountType findByName(@PathVariable String name) {
+        logger.info("input name: {}", name);
+        return accountTypeService.findByName(name);
+    }
+
+}
---
+++ src/test/java/tw/com/rex/accountbook/controller/AccountTypeControllerTest.java
@@ -0,0 +1,48 @@
+package tw.com.rex.accountbook.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import tw.com.rex.accountbook.repository.dao.AccountType;
+import tw.com.rex.accountbook.service.AccountTypeService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class AccountTypeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private AccountTypeService accountTypeService;
+
+    @Test
+    public void testFindByName() throws Exception {
+
+        AccountType entity = AccountType.of("銀行");
+
+        given(accountTypeService.findByName(entity.getName())).willReturn(entity);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get(
+                "/accountType/find/name/" + entity.getName())//
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)//
+                .andExpect(status().isOk())//
+                .andExpect(jsonPath("$.name", is(entity.getName())));
+    }
+
+}
